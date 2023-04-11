e7fe2ad4b148fbd57070c18cc9f8d0a0aa39932f
Mitka Panarin <mitka_j@yahoo.com>
2023-04-11 12:53:59 +0200

    .

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { readDataFromFile, writeDataToFile } from "../utils/files.js";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 
 const databaseName = "dataBase.json";
 export const UserRouter = express.Router();
@@ -25,19 +25,67 @@ UserRouter.post("/create-users", (req, res) => {
             message: "cannot write data to database",
           });
         } else {
-          res.json(newData)
+          res.json(newData);
         }
       });
     }
   });
 });
 UserRouter.delete("/delete-users/:id", (req, res) => {
-  const {id} = req.params
-
-  readDataFromFile
+  const id = req.params.id;
+  readDataFromFile(databaseName, (err, data) => {
+    if (err) {
+      res.json({ message: "Unable to access database" });
+    } else {
+      const oldData = JSON.parse(data);
+      const filteredData = oldData.filter((item) => item.id !== id);
+      if (oldData.length === filteredData.length) {
+        res.json({ message: "User not found" });
+      } else {
+        writeDataToFile(databaseName, JSON.stringify(filteredData), (err) => {
+          if (err) {
+            res.json({ message: "Failed to delete user" });
+          } else {
+            res.json({ message: "User deleted successfully" });
+          }
+        });
+      }
+    }
+  });
 });
-UserRouter.patch("/update-users", (req, res) => {});
-UserRouter.get("/get-one-user", (req, res) => {});
+
+UserRouter.get("/get-one-user/:id", (req, res) => {
+  const userId = req.params.id;
+  readDataFromFile(databaseName, (err, data) => {
+    if (err) {
+      res.json({
+        message: "unable to access database",
+      });
+    } else {
+      const users = JSON.parse(data);
+      const user = users.find((item) => item.id === userId);
+      if (user) {
+        res.json(user);
+      } else {
+        res.json({
+          message: "user not found",
+        });
+      }
+    }
+  });
+});
+
+UserRouter.get("/get-one-user/:id", (req, res) => {
+  readDataFromFile(databaseName, (err, data) => {
+    if (err) {
+      res.json({
+        message: "unable to access database",
+      });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
 UserRouter.get("/get-all-users", (req, res) => {
   readDataFromFile(databaseName, (err, data) => {
     if (err) {
