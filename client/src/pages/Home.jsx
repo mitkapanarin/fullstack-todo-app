f27ee6d32f44240aaffe12381d870c7a099861fd
Mitka Panarin <mitka_j@yahoo.com>
2023-04-28 21:44:07 +0200

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+   <head>
+    <title>My App Home</title>
+  </head>
+  {/* <body>
+    <h1>Welcome, [user name]!</h1>
+    <p>Here's a summary of your recent activity:</p>
+    <ul>
+      <li>Completed task A</li>
+      <li>Completed task B</li>     
+    </ul>
+    <p>Quick access links:</p>
+    <ul>
+      <li><a href="/all-tasks">View tasks</a></li>
+      <li><a href="/update-user">Update your profile</a></li>
+    </ul>
+  </body> */}
+    </div>
   )
 }
 
